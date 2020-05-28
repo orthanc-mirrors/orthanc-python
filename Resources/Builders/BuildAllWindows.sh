5e99030d38b325a76f035a6984997e168fffb6c7
Sebastien Jodogne <s.jodogne@gmail.com>
2020-05-28 21:02:23 +0200

    Resources/Builders/BuildAllWindows.sh

---
+++ Resources/Builders/BuildAllWindows.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -e
+
+if [ "$#" -ne 1 ]; then
+    echo "Illegal number of parameters: Must provide the target folder"
+    exit -1
+fi
+
+# Determine the absolute path to the directory containing the script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+TARGET=$1
+echo "Will publish libraries in folder: ${TARGET}"
+
+mkdir -p ${TARGET}
+
+VERSION=`grep "set(PLUGIN_VERSION" ${DIR}/../../CMakeLists.txt | cut -d '"' -f 2`
+echo "Version of the Python plugin: ${VERSION}"
+
+
+set -x
+
+bash ${DIR}/MinGW32-Python2.7/docker-compile.sh Release
+cp ${DIR}/../../docker-build/lib/libOrthancPython.dll ${TARGET}/OrthancPython-Win32-Python2.7-${VERSION}.dll
+
+bash ${DIR}/MinGW32-Python3.7/docker-compile.sh Release
+cp ${DIR}/../../docker-build/lib/libOrthancPython.dll ${TARGET}/OrthancPython-Win32-Python3.7-${VERSION}.dll
+
+bash ${DIR}/MinGW32-Python3.8/docker-compile.sh Release
+cp ${DIR}/../../docker-build/lib/libOrthancPython.dll ${TARGET}/OrthancPython-Win32-Python3.8-${VERSION}.dll
+
+bash ${DIR}/MinGW64-Python2.7/docker-compile.sh Release
+cp ${DIR}/../../docker-build/lib/libOrthancPython.dll ${TARGET}/OrthancPython-Win64-Python2.7-${VERSION}.dll
+
+bash ${DIR}/MinGW64-Python3.7/docker-compile.sh Release
+cp ${DIR}/../../docker-build/lib/libOrthancPython.dll ${TARGET}/OrthancPython-Win64-Python3.7-${VERSION}.dll
+
+bash ${DIR}/MinGW64-Python3.8/docker-compile.sh Release
+cp ${DIR}/../../docker-build/lib/libOrthancPython.dll ${TARGET}/OrthancPython-Win64-Python3.8-${VERSION}.dll
