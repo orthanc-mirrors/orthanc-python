8f64614c089f8d5ca112ab4bd61739ea33a3329b
Sebastien Jodogne <s.jodogne@gmail.com>
2023-03-28 17:27:06 +0200

    added Windows builders for Python 3.9, 3.10 and 3.11
    --HG--
    branch : OrthancPython-4.0

--- Resources/Builders/MinGW32-Python3.7/docker-internal.sh
+++ Resources/Builders/MinGW32-Python3.7/docker-internal.sh
@@ -10,7 +10,7 @@ cp -r /source/Resources /tmp/source-writeable/
 mkdir /tmp/build
 cd /tmp/build
 
-wget https://orthanc.osimis.io/ThirdPartyDownloads/Python/python-3.7.7-win32.zip
+wget https://orthanc.uclouvain.be/third-party-downloads/Python/python-3.7.7-win32.zip
 unzip python-3.7.7-win32.zip
 
 cmake /tmp/source-writeable/ \
---
+++ Resources/Builders/MinGW64-Python3.11/docker-compile.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -ex
+
+if [ "$1" != "Debug" -a "$1" != "Release" ]; then
+    echo "Please provide build type: Debug or Release"
+    exit -1
+fi
+
+if [ -t 1 ]; then
+    # TTY is available => use interactive mode
+    DOCKER_FLAGS='-i'
+fi
+
+ROOT_DIR=`dirname $(readlink -f $0)`/../../..
+
+mkdir -p ${ROOT_DIR}/docker-build/
+
+( cd ${ROOT_DIR}/Resources/Builders/ && \
+        docker build \
+               -f ./Dockerfile-MinGW-BuildEnvironment \
+               -t mingw-python-build . )
+
+docker run -t ${DOCKER_FLAGS} --rm \
+    --user $(id -u):$(id -g) \
+    -v ${ROOT_DIR}:/source:ro \
+    -v ${ROOT_DIR}/docker-build:/target:rw \
+    mingw-python-build \
+    bash /source/Resources/Builders/MinGW64-Python3.11/docker-internal.sh $1
+
+ls -lR ${ROOT_DIR}/docker-build/
