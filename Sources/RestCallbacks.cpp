05c74b709d7aa4921d417656140c60342644fa66
Sebastien Jodogne <s.jodogne@gmail.com>
2024-05-30 22:27:45 +0200

    updated copyright, as Orthanc Team now replaces Osimis

--- Sources/RestCallbacks.cpp
+++ Sources/RestCallbacks.cpp
@@ -1,6 +1,7 @@
 /**
  * Python plugin for Orthanc
- * Copyright (C) 2020-2024 Osimis S.A., Belgium
+ * Copyright (C) 2020-2023 Osimis S.A., Belgium
+ * Copyright (C) 2024-2024 Orthanc Team SRL, Belgium
  * Copyright (C) 2021-2024 Sebastien Jodogne, ICTEAM UCLouvain, Belgium
  *
  * This program is free software: you can redistribute it and/or
